83ed666de0b8e86f801ef4a106496c3fd8d33ad9
anthonydanger <anthony.sanchez.sf@gmail.com>
2014-01-25 11:23:13 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beatboxcashier::Application.config.secret_key_base = 'f2671be0b867d9b8803e9dee8794f082d6629fb63751b9a141a18d41c1866c6e2aac61d564366cee86f643d196a5eab3fbb5170d9b5127bfb8977af9847bdaf4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beatboxcashier::Application.config.session_store :cookie_store, key: '_beatboxcashier_session'
